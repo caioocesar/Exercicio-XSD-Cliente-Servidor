a061e799178cb0546da8f3c7a3834c57f8186b03
Caio César <caio.cesarsampaio@hotmail.com>
2019-05-07 18:56:50 -0300

    Funções de imprimir
    Nova versão da função imprimir xml e nova função de exibir o xml em forma bonitinha de página html

--- Cliente-Python/visualizadorHistorico.py
+++ Cliente-Python/visualizadorHistorico.py
@@ -8,6 +8,7 @@ from sys import exit
 from socket import *
 from threading import *
 import time
+import os
 
 TESTE = False
 
@@ -190,52 +191,150 @@ def carregarXSD(nome_arq):
     # Método da biblioteca que Guarda que o arquivo lido é um XML Schema que vai ser usado para validação
     return etree.XMLSchema(xsd_doc)
 
-def imprimir(XMLdoHistorico):  # passar nome do arquivo xml ja salvo em memoria!! Nao adianta passar string e tentar usar fromstring ou fazer aqui o arquivo
-
-    #xml_arq = etree.parse(XMLdoHistorico)
-    #xml_arq = etree.fromstring(XMLdoHistorico)
+def imprimir(XMLdoHistorico): #parametro: string
+    
+    #xml_arq = open(XMLdoHistorico, "r",-1,"utf-8")
     xml_arq = etree.parse(StringIO(XMLdoHistorico))
     xml = xml_arq.getroot()
-
+    
     print("------------------------------------------------------------------------")
-    print(xml[0][0].text)
-    print(xml[0][1].text)
-    print("Curso: " + xml[1].text)
-    print("Aluno: " + xml[2].text)
-    print("Matricula: " + xml[3].text)
-    print("Cr medio: " + xml[4].text)
-    print("Data geracao: " + xml[5].text)
-    print("Hora geracao: " + xml[6].text)
-    print("Cod autenticacao: " + xml[7].text)
+    print(xml.find('universidade').find('nome').text)
+    print(xml.find('universidade').find('abreviacao').text)
+    print("Curso: "+xml.find('curso').text)
+    print("Aluno: "+xml.find('aluno').text)
+    print("Matricula: "+xml.find('matricula').text)
+    print("Cr medio: "+xml.find('crMedio').text)
+    print("Data geracao: "+xml.find('dataGeracao').text)
+    print("Hora geracao: "+xml.find('horaGeracao').text)
+    print("Cod autenticacao: "+xml.find('codigoAutenticacao').text)
     print("------------------------------------------------------------------------")
     print("\n")
-    listaPeriodos = xml[8].findall('Periodo')
+    listaPeriodos = xml.find('periodos').findall('Periodo')
     for i in range(len(listaPeriodos)):
-        print("Ano Semestre: " + listaPeriodos[i][0].text)
-        print("Creditos solicitados: " + listaPeriodos[i][1].text)
-        print("Creditos acumulados: " + listaPeriodos[i][2].text)
-        print("Creditos obtidos: " + listaPeriodos[i][3].text)
-        print("Cr periodo: " + listaPeriodos[i][4].text)
+        print("Ano Semestre: "+listaPeriodos[i].find('anoSemestre').text)
+        print("Creditos solicitados: "+listaPeriodos[i].find('creditosSolicitados').text)
+        print("Creditos acumulados: "+listaPeriodos[i].find('creditosAcumulados').text)
+        print("Creditos obtidos: "+listaPeriodos[i].find('creditosObtidos').text)
+        print("Cr periodo: "+listaPeriodos[i].find('crPeriodo').text)
         print("\n")
-
-        listaDisciplinasAA = listaPeriodos[i][5].findall('AtividadeAcademica')
+        
+        listaDisciplinasAA = listaPeriodos[i].find('disciplinas').findall('AtividadeAcademica')
         for j in range(len(listaDisciplinasAA)):
-            print("\tCodigo disciplina: " + listaDisciplinasAA[j][0].text)
-            print("\tNome disciplina: " + listaDisciplinasAA[j][1].text)
-            print("\tCreditos: " + listaDisciplinasAA[j][2].text)
-            print("\tNota: " + listaDisciplinasAA[j][3].text)
-            print("\tSituacao: " + listaDisciplinasAA[j][4].text)
+            print("\tCodigo disciplina: "+listaDisciplinasAA[j].find('codigo').text)
+            print("\tNome disciplina: "+listaDisciplinasAA[j].find('nome').text)
+            print("\tCreditos: "+listaDisciplinasAA[j].find('creditos').text)
+            print("\tNota: "+listaDisciplinasAA[j].find('nota').text)
+            print("\tSituacao: "+listaDisciplinasAA[j].find('situacaoAA').text)
             print("\n")
-
-        listaDisciplinas = listaPeriodos[i][5].findall('Disciplina')
+            
+        listaDisciplinas = listaPeriodos[i].find('disciplinas').findall('Disciplina')
         for j in range(len(listaDisciplinas)):
-            print("\tCodigo disciplina: " + listaDisciplinas[j][0].text)
-            print("\tNome disciplina: " + listaDisciplinas[j][1].text)
-            print("\tCreditos: " + listaDisciplinas[j][2].text)
-            print("\tNota: " + listaDisciplinas[j][3].text)
-            print("\tSituacao: " + listaDisciplinas[j][4].text)
+            print("\tCodigo disciplina: "+listaDisciplinas[j].find('codigo').text)
+            print("\tNome disciplina: "+listaDisciplinas[j].find('nome').text)
+            print("\tCreditos: "+listaDisciplinas[j].find('creditos').text)
+            print("\tNota: "+listaDisciplinas[j].find('nota').text)
+            print("\tSituacao: "+listaDisciplinas[j].find('situacao').text)
             print("\n")
-
+            
         print("------------------------------------------------------------------------")
+
+
+def geraHtml(XMLdoHistorico): #parametro: string
+    
+    #xml_arq = open(XMLdoHistorico, "r",-1,"utf-8")
+    xml_arq = etree.parse(StringIO(XMLdoHistorico))
+    xml = xml_arq.getroot()
+    texto = []
+    name = "historico"
+    name = name + '.html'
+    arq = open(name, 'w',-1,"utf-8")
+
+    print('Gerando',name,'...\n')
+
+    
+    texto.append("<!DOCTYPE html>\n<html lang='pt-BR'>\n\n<html>\n\n")
+    texto.append("\t<head>\n\t\t<title>Histórico</title>\n\t\t<meta charset = 'utf-8'>\n\t\t<link rel=\"stylesheet\" type=\"text/css\" href=\"styles.css\">\n"+"\t\t<link rel=\"shortcut icon\" href=\"ufrrj.jpg\" type=\"image/jpg\"/>\n\t</head>\n")
+    texto.append("\n\t<body>\n")
+    texto.append("\n\t\t<header>\n")
+    texto.append("\t\t<a href=\"http://portal.ufrrj.br\" title=\"UFRRJ\"><img src=\"ufrrj.jpg\" class = \"imagem\" align = \"left\" alt=\"Falha na imagem\"></a>\n")
+    texto.append("\t\t<h1><br>"+xml.find('universidade').find('nome').text+"</h1>\n")
+    texto.append("\t\t<h1>"+xml.find('universidade').find('abreviacao').text+"</h1><br><br>\n")
+    texto.append("\t\t</header>\n\n")
+    texto.append("\n\t\t<div>\n")
+    texto.append("\t\t<img src=\"perfil.jpg\" class = \"imagemPerfil\" align = \"left\" title=\"Perfil\" alt=\"Falha na imagem\"><br>Curso: "+xml.find('curso').text+"<br>\n")
+    texto.append("\t\tAluno: "+xml.find('aluno').text+"<br>\n")
+    texto.append("\t\tMatrícula: "+xml.find('matricula').text+"<br>\n")
+    texto.append("\t\tCR médio: "+xml.find('crMedio').text+"<br>\n")
+    texto.append("\t\tData geração: "+xml.find('dataGeracao').text+"<br>\n")
+    texto.append("\t\tHora geração: "+xml.find('horaGeracao').text+"<br>\n")
+    texto.append("\t\tCód. autenticação: "+xml.find('codigoAutenticacao').text+"<br><br>\n")
+    texto.append("\t\t</div>\n")
+
+
+    texto.append("\n\t\t<section>\n")
+    listaPeriodos = xml.find('periodos').findall('Periodo')
+    texto.append("\t\t\t<img src=\"legenda.png\" class = \"imagemLegenda\" align = \"right\" title=\"legenda\" alt=\"Falha na imagem\">\n")
+    for i in range(len(listaPeriodos)):
+        texto.append("\t\t\t<br><br><table>\n")#\n\t\t\t<tr><th>Ano Semestre</th><th>Creditos solicitados</th><th>Creditos acumulados</th><th>Creditos obtidos</th><th>Cr periodo</th></tr><br>\n")
+        texto.append("\t\t\t<tr><th>"+listaPeriodos[i].find('anoSemestre').text+"</th></tr>\n")
+        
+        texto.append("\t\t\t\t<tr><th>Código disciplina</th>"+"<th>Nome disciplina</th>"+"<th>Créditos</th>"+"<th>Nota</th>"+"<th>Situação</th></tr>\n")
+        
+        listaDisciplinasAA = listaPeriodos[i].find('disciplinas').findall('AtividadeAcademica')
+        for j in range(len(listaDisciplinasAA)):
+            texto.append("\t\t\t\t<tr><td>"+listaDisciplinasAA[j].find('codigo').text+"</td>\n")
+            texto.append("\t\t\t\t<td>"+listaDisciplinasAA[j].find('nome').text+"</td>\n")
+            texto.append("\t\t\t\t<td>"+listaDisciplinasAA[j].find('creditos').text+"</td>\n")
+            texto.append("\t\t\t\t<td>"+listaDisciplinasAA[j].find('nota').text+"</td>\n")
+            texto.append("\t\t\t\t<td>"+listaDisciplinasAA[j].find('situacaoAA').text+"</td></tr>\n\n")
+
+            
+        listaDisciplinas = listaPeriodos[i].find('disciplinas').findall('Disciplina')
+        for j in range(len(listaDisciplinas)):
+            texto.append("\t\t\t\t<tr><td>"+listaDisciplinas[j].find('codigo').text+"</td>\n")
+            texto.append("\t\t\t\t<td>"+listaDisciplinas[j].find('nome').text+"</td>\n")
+            texto.append("\t\t\t\t<td>"+listaDisciplinas[j].find('creditos').text+"</td>\n")
+            texto.append("\t\t\t\t<td>"+listaDisciplinas[j].find('nota').text+"</td>\n")
+            texto.append("\t\t\t\t<td>"+listaDisciplinas[j].find('situacao').text+"</td></tr>\n\n")
+
+        texto.append("\t\t\t</tr><td></td><td>Créditos solicitados: "+listaPeriodos[i].find('creditosSolicitados').text+"</td>\n")
+        texto.append("\t\t\t<td>Créditos acumulados: "+listaPeriodos[i].find('creditosAcumulados').text+"</td>\n")
+        texto.append("\t\t\t<td>Créditos obtidos: "+listaPeriodos[i].find('creditosObtidos').text+"</td>\n")
+        texto.append("\t\t\t<td>CR período: "+listaPeriodos[i].find('crPeriodo').text+"</td></tr>\n\n")
+        texto.append("\t\t\t</table>\n\n")
+
+    texto.append("\n\t\t</section>\n")
+    texto.append("\n\t\t<footer>\n")
+    texto.append("\t\t\t<br>Grupinho de TEDB, 2019.")
+    texto.append("\n\t\t</footer>\n")
+    texto.append("\t</body>\n")
+    texto.append("\n</html>")
+
+            
+
+
+    arq.writelines(texto)
+
+    print(name,"gerado com sucesso.\nSalvo em:", os.path.abspath(name))
+
+    arq.close()
+
+    flag = False
+
+    while(flag == False):
+
+    	option = input("\nDeseja abrir o histórico gerado? (S / N)\n")
+
+    	if(option == 's' or option == 'S' or option == 'sim' or option == 'yes' or option == 'y'):
+    		os.startfile(os.path.abspath(name))
+    		flag = True
+
+    	elif(option == 'n' or option == 'N' or option == 'nao' or option == 'not' or option == 'no'):
+    		flag = True
+
+    	if(flag == False):
+    		print("Opção inválida. Tente novamente...")
+
+    print("Visualização encerrada.")
 
 run()
